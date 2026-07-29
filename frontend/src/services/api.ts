7505ecffdcf2fd9bcc07f78740c2f0d0239f6a15
sahilaslam-art <sahilaslam754@gmail.com>
2026-07-29 12:28:13 +0530

    Updated API URL for production

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = (import.meta as any).env?.VITE_API_URL || 'http://localhost:5000/api/v1';
+const API_URL = (import.meta as any).env?.VITE_API_URL || 'https://instagram-clone-project-backend.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
